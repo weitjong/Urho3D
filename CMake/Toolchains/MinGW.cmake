07774423f680adbfe94f47cacbd75b84a314a505
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2020-04-09 00:11:33 +0800

    Ensure rooted package is being found when cross-compiling.

--- CMake/Toolchains/MinGW.cmake
+++ CMake/Toolchains/MinGW.cmake
@@ -130,6 +130,7 @@ set (CMAKE_FIND_ROOT_PATH ${MINGW_SYSROOT})     # Intentionally do not use CMAKE
 set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set (CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 # Workaround try_compile() limitation where it cannot yet see cache variables during initial configuration
 if (NOT IN_TRY_COMPILE)
